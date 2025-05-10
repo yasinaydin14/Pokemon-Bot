5951cc6794d31f947d5bd1b8688f1290c62176bb
jakegrigsby <jcg6dn@virginia.edu>
2025-05-10 08:46:08 -0500

    upgrade ParsedReplayDataset filtering

--- metamon/data/replay_dataset/parsed_replays/loading.py
+++ metamon/data/replay_dataset/parsed_replays/loading.py
@@ -1,11 +1,12 @@
 import os
 import json
-import random
 from typing import Optional
+from datetime import datetime
+from collections import defaultdict
 
 import torch
 from torch.utils.data import Dataset
-import numpy as np
+import tqdm
 
 from metamon.interface import ObservationSpace, RewardFunction, UniversalState
 
@@ -16,28 +17,65 @@ class ParsedReplayDataset(Dataset):
         dset_root: str,
         observation_space: ObservationSpace,
         reward_function: RewardFunction,
-        formats: list[str],
+        formats: Optional[list[str]] = None,
         wins_losses_both: str = "both",
+        min_rating: Optional[int] = None,
+        max_rating: Optional[int] = None,
+        min_date: Optional[datetime] = None,
+        max_date: Optional[datetime] = None,
+        verbose: bool = False,
     ):
         assert dset_root is not None and os.path.exists(dset_root)
-        assert wins_losses_both in ["wins", "losses", "both"]
+        formats = formats or [
+            f"gen{g}{t}" for g in range(1, 5) for t in ["ou", "uu", "nu", "ubers"]
+        ]
         self.observation_space = observation_space
         self.reward_function = reward_function
         self.filenames = []
+
+        def _rating_to_int(rating: str) -> int:
+            try:
+                return int(rating)
+            except ValueError:
+                return 1000
+
+        bar = lambda it, desc: (
+            it if not verbose else tqdm.tqdm(it, desc=desc, colour="red")
+        )
+
         for format in formats:
             path = os.path.join(dset_root, format)
-            for filename in os.listdir(path):
-                if wins_losses_both == "wins" and not "WIN" in filename:
+            for filename in bar(os.listdir(path), desc=f"Finding {format} battles"):
+                if not filename.endswith(".json"):
                     continue
-                elif wins_losses_both == "losses" and not "LOSS" in filename:
+                battle_id, rating, p1_name, _, p2_name, mm_dd_yyyy, result = filename[
+                    :-5
+                ].split("_")
+                rating = _rating_to_int(rating)
+                date = datetime.strptime(mm_dd_yyyy, "%m-%d-%Y")
+                battle_id = (
+                    battle_id.replace("[", "").replace("]", "").replace(" ", "").lower()
+                )
+                if (
+                    format not in battle_id
+                    or (min_rating is not None and rating < min_rating)
+                    or (max_rating is not None and rating > max_rating)
+                    or (min_date is not None and date < min_date)
+                    or (max_date is not None and date > max_date)
+                    or (wins_losses_both == "wins" and result != "WIN")
+                    or (wins_losses_both == "losses" and result != "LOSS")
+                ):
                     continue
                 self.filenames.append(os.path.join(path, filename))
-        print(f"Dataset contains {len(self.filenames)} battles...")
+
+        if verbose:
+            print(f"Dataset contains {len(self.filenames)} battles")
 
     def __len__(self):
         return len(self.filenames)
 
     def __getitem__(self, i):
+        breakpoint()
         with open(self.filenames[i], "r") as f:
             data = json.load(f)
         states = [UniversalState.from_dict(s) for s in data["states"]]
@@ -61,8 +99,7 @@ if __name__ == "__main__":
         dset_root="/mnt/nfs_client/jake/metamon_parsed_hf_replays",
         observation_space=DefaultObservationSpace(),
         reward_function=DefaultShapedReward(),
-        formats=["gen1ou"],
+        verbose=True,
     )
     print(len(dset))
     obs, actions, rewards, dones = dset[0]
-    breakpoint()
