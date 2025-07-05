d83be1fbb834cfaf72180932be155eec4bad18be
jakegrigsby <jcg6dn@virginia.edu>
2025-07-05 17:04:05 -0500

    illegal action masking prompts patch to 'recharge' edge case in metamon battle

--- metamon/env/metamon_battle.py
+++ metamon/env/metamon_battle.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from logging import Logger
 from typing import Any, Dict, List, Optional, Tuple
-
+import warnings
 
 import poke_env.environment as pe
 
@@ -129,16 +129,11 @@ class MetamonBackendBattle(pe.AbstractBattle):
 
         active_pokemon = None
         side = request.get("side", False)
-        if side and not self.trapped and not self.reviving:
+        if side and not self.reviving:
             active_pokemon = self._update_turn_from_side_request(side)
 
         active = request.get("active", False)
-        if (
-            active
-            and active_pokemon is not None
-            and not self.trapped
-            and not self.reviving
-        ):
+        if active and active_pokemon is not None and not self.reviving:
             self._update_turn_from_active_request(active[0], active_pokemon)
 
     def _parse_condition_from_side_request(
@@ -279,7 +274,7 @@ class MetamonBackendBattle(pe.AbstractBattle):
                     "mimic",
                 }
                 if not plausible_reasons_to_discover.intersection(known_had_moves):
-                    raise mrp.exceptions.ForwardException(
+                    warnings.warn(
                         f"Unknown move {move_name} (lookup_name: {move_id}) discovered with known_had_moves: {known_had_moves}, known_active_moves: {known_active_moves}"
                     )
                 move = Move(move_name, gen=self._gen)
