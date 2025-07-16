f2cb08273d111e7da79863af3b3201e2159a02bb
Jake Grigsby <grigsby@cs.utexas.edu>
2025-07-16 13:53:16 -0500

    fix old version dset loading

--- metamon/data/__init__.py
+++ metamon/data/__init__.py
@@ -2,5 +2,5 @@ import os
 
 DATA_PATH = os.path.dirname(__file__)
 
-from metamon.data.datasets import ParsedReplayDataset
+from .parsed_replay_dset import ParsedReplayDataset
 from . import raw_replay_util
